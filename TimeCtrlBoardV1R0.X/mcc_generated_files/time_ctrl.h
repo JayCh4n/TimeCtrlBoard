da8e6beefea036980efe0b1095d6f5816a34e7de
JayCh4n <biubiuboom@outlook.com>
2018-06-12 11:04:29 +0800

    接

---
+++ TimeCtrlBoardV1R0.X/mcc_generated_files/time_ctrl.h
@@ -0,0 +1,31 @@
+/* 
+ * File:   time_ctrl.h
+ * Author: Sumson
+ *
+ * Created on 2018?6?12?, ??9:14
+ */
+
+#ifndef TIME_CTRL_H
+#define	TIME_CTRL_H
+
+#include "eusart.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define TIME_CTRL_STOP  0x1C
+#define TIME_CTRL_START 0x1D
+
+extern uint16_t timeCtrlValue[8][4];
+extern uint8_t timeCtrlStartFlag;
+extern uint8_t timeCtrlOvertimeMask;
+
+
+void tempCtrlProcess(void);
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIME_CTRL_H */
+
