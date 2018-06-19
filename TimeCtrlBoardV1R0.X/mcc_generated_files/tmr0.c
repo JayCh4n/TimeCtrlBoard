8a369e9eccb108b5ec18e24c3c24d1440ee224a3
JayCh4n <biubiuboom@outlook.com>
2018-06-19 18:17:33 +0800

    定时器0   1：256分频改为1：32分频   定时时间更加精确

--- TimeCtrlBoardV1R0.X/mcc_generated_files/tmr0.c
+++ TimeCtrlBoardV1R0.X/mcc_generated_files/tmr0.c
@@ -65,14 +65,14 @@ void TMR0_Initialize(void)
 {
     // Set TMR0 to the options selected in the User Interface
 	
-    // PSA assigned; PS 1:256; TMRSE Increment_hi_lo; mask the nWPUEN and INTEDG bits
-    OPTION_REG = (uint8_t)((OPTION_REG & 0xC0) | 0xD7 & 0x3F); 
+    // PSA assigned; PS 1:32; TMRSE Increment_hi_lo; mask the nWPUEN and INTEDG bits
+    OPTION_REG = (uint8_t)((OPTION_REG & 0xC0) | 0xD4 & 0x3F); 
 	
-    // TMR0 240; 
-    TMR0 = 0xF0;
+    // TMR0 131; 
+    TMR0 = 0x83;
 	
     // Load the TMR value to reload variable
-    timer0ReloadVal= 240;
+    timer0ReloadVal= 131;
 
     // Clear Interrupt flag before enabling the interrupt
     INTCONbits.TMR0IF = 0;
